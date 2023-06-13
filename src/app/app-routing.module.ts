822e2a56c42c6ef8837bf4642474695c5969b91c
Mohamed Amine Hamdi <mohamedamine.hamdi@esprit.tn>
2023-06-13 18:57:08 +0100

    first commit template front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,44 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllTemplateAdminComponent } from './backoffice/all-template-admin/all-template-admin.component';
+import { BodyAdminComponent } from './backoffice/body-admin/body-admin.component';
+import { BodyUserComponent } from './frontoffice/body-user/body-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+
+  path : 'admin', component : AllTemplateAdminComponent,
+children : [
+  {
+  path : 'home',
+  component : BodyAdminComponent,
+  }
+          ]
+},
+{
+  path :'',component : AllTemplateAdminComponent,
+  children:[
+    {
+      path : '',
+      component:BodyUserComponent,
+    }
+
+  ]
+
+
+}
+                      ];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
+
+
+
+
 export class AppRoutingModule { }
